663facb0430498b930e5d421b38d6868acb7f6da
Grant Archibald <31553604+Grant-Archibald-MS@users.noreply.github.com>
2023-06-26 13:49:51 -0700

    DataverseExecute GET/POST example

--- src/PowerApps-TestEngine-Modules/testengine.module.dataverse/DataverseExecuteFunction.cs
+++ src/PowerApps-TestEngine-Modules/testengine.module.dataverse/DataverseExecuteFunction.cs
@@ -19,24 +19,33 @@ namespace testengine.module
         private readonly ITestInfraFunctions _testInfraFunctions;
 
         public DataverseExecuteFunction(ILogger logger, ITestInfraFunctions testInfraFunctions, ITestState state)
-            : base("DataverseExecute", BlankType.String, FormulaType.String, FormulaType.String)
+            : base("DataverseExecute", BlankType.String, FormulaType.String, FormulaType.String, FormulaType.String)
         {
             _logger = logger;
             _state = state;
             _testInfraFunctions = testInfraFunctions;
         }
 
-        public BlankValue Execute(StringValue name, StringValue action)
+        public BlankValue Execute(StringValue name, StringValue method, StringValue action)
         {
             _logger.LogInformation("------------------------------\n\n" +
                 "Executing Dataverse Execute function.");
 
-            var url = $"/api/data/v9.2/{action.Value}";
+            var url = $"/api/data/v9.1/{action.Value}";
             var variable = name.Value;
 
             var page = _testInfraFunctions.GetContext().Pages.First();
-            page.EvaluateAsync($"fetch('{url}').then(response=>response.json()).then(data=>{{ if (typeof document.Dataverse === 'undefined') {{ document.Dataverse = {{}} }} document.Dataverse['{variable}'] = JSON.stringify(data); }})").Wait();
+            string fetchParams = "{}";
+            switch ( method.Value )
+            {
+                case "POST":
+                    // TODO Handle parameters
+                    fetchParams = $"{{method: \"POST\", headers: {{ \"Content-Type\": \"application/json\" }}, body:\"{{}}\" }}";
+                    break;
+            }
+            page.EvaluateAsync($"fetch('{url}', {fetchParams}).then(response=>response.json()).then(data=>{{ if (typeof document.Dataverse === 'undefined') {{ document.Dataverse = {{}} }} document.Dataverse['{variable}'] = JSON.stringify(data); }})").Wait();
 
+            //TODO handle error
             _logger.LogInformation("Successfully executing Dataverse Execute function.");
 
             return BlankValue.NewBlank();
